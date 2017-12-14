854996f33be95eb70df876f8831b9be093e2a5d1
Alexander Klee <aklee@pdc-online.com>
2017-12-14 14:02:54 +0100

    Add Process namespace and add default Templates

--- Pdc.System/Process/ITransmitterProcess.cs
+++ Pdc.System/Process/ITransmitterProcess.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pdc.System.Process
+﻿namespace Pdc.System.Process
 {
     /// <summary>
-    /// Represents a process running within a Transmitter. 
+    /// Represents a process running within a Transmitter.
     /// </summary>
     // #refactor Extract to Pdc.Data.Process namespace
     public interface ITransmitterProcess
     {
         void Run();
+
         int Errors { get; }
     }
 }
