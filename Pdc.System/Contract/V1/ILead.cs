28745269b039b9ef12b542b95d693565c75e4166
Alexander Klee <aklee@pdc-online.com>
2017-12-16 19:21:35 +0100

    Refactor the ILead Contract

---
+++ Pdc.System/Contract/V1/ILead.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Pdc.System.Contract.V1
+{
+
+    interface ILead
+    {
+        int Id { get; set; }
+
+        [JsonProperty(PropertyName = "id", Required = Required.Always)]
+        int LeadId { get; set; }
+
+        [JsonProperty(PropertyName = "name", Required = Required.Always)]
+        string Name { get; set; }
+    }
+}
