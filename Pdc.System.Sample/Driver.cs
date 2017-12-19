aacf98083206b15b92e458a47dea562bfe1cfc4b
Alexander Klee <aklee@pdc-online.com>
2017-12-19 10:36:48 +0100

    Prepare sample implementation of Component pattern

--- Pdc.System.Sample/Driver.cs
+++ Pdc.System.Sample/Driver.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pdc.System.Sample
 {
-    class Program
+    class Driver
     {
         static void Main(string[] args)
         {
